e06ba17047a43c8157903b753d0495a227ae3169
tevin-otieno <tevin.otieno@student.moringaschool.com>
2021-11-14 11:37:53 +0300

    add a better way to display exeption

--- src/main/java/exceptions/ApiException.java
+++ src/main/java/exceptions/ApiException.java
@@ -1,2 +1,15 @@
-package exceptions;public class ApiException {
+package exceptions;
+
+public class ApiException extends RuntimeException{
+
+    private final int statusCode;
+
+    public ApiException (int statusCode, String msg){
+        super(msg); //Caused by
+        this.statusCode = statusCode;
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
 }
