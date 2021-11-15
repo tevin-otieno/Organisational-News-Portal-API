07f2909bc225c61d01e05579f05ceae8bc32485c
tevin-otieno <tevin.otieno@student.moringaschool.com>
2021-11-15 08:10:52 +0300

    add the data access objects in the application

--- src/main/java/dao/UsersDao.java
+++ src/main/java/dao/UsersDao.java
@@ -1,2 +1,26 @@
-package dao;public class UsersDao {
+package dao;
+
+import models.Departments;
+import models.Users;
+
+import java.util.List;
+
+public interface UsersDao {
+
+    //create
+
+    void  add(Users user);
+
+    //read
+
+    List<Users> getAll();
+    List<Departments> getAllUserDepartments(int user_id);
+    Users findById(int id);
+
+    //update
+
+    //delete
+
+    void clearAll();
+
 }
